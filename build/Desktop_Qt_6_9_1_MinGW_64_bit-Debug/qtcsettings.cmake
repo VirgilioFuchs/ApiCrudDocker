b467ac11852670406550af5f8cbfc977d0553b53
Virgílio Fuchs <148342758+VirgilioFuchs@users.noreply.github.com>
2025-08-29 09:02:42 -0300

    Deletando a pasta build do git

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/qtcsettings.cmake
+++
@@ -1,3 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/Anglo/Documents/LoginSenhaTesteQT/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
